22d9f1ac90e4384a8d979fefc041ddb096f09ce2
Szymon Roszyk <roszyk.szymon@gmail.com>
2023-05-13 14:56:57 +0200

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [display, setDisplay] = useState("");
+  const [number1, setNumber1] = useState(undefined);
+  const [number2, setNumber2] = useState(undefined);
+  const [result, setResult] = useState(0);
+  const [numberInput1, setNumberInput1] = useState(undefined);
+  const [numberInput2, setNumberInput2] = useState(undefined);
+  const [resultInput, setResultInput] = useState(0);
+
+
+  function handleClick(number) {
+    setDisplay(display + number);
+  }
+
+  function handleAdd() {
+    if (number1 === undefined) {
+      setNumber1(parseInt(display));
+    }
+    else {
+      setNumber2(parseInt(display));
+    }
+
+    setDisplay("");
+  }
+
+  function handleCalc() {
+    setResult(number1 + number2);
+  }
+
+  function handleInput1(ev){
+    setNumberInput1(parseInt(ev.target.value));
+  }
+
+  function handleInput2(ev){
+    setNumberInput2(parseInt(ev.target.value));
+  }
+
+  function handleResultInput(){
+    setResultInput(numberInput1 + numberInput2);
+  }
+
+  const btns = [];
+  for (let i = 0; i <= 9; i++) {
+    btns.push(<button key={i} onClick={() => handleClick(i)}>{i}</button>);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{display}</div>
+      {btns}
+      <button onClick={handleAdd}>+</button>
+      <button onClick={handleCalc}>=</button>
+      <div>{number1} + {number2} = {result}</div>
+
+      <input onChange={handleInput1} type='number'></input>
+      +
+      <input onChange={handleInput2} type='number'></input>
+      =
+      {resultInput}
+      <button onClick={handleResultInput}>=</button>
+    </>
   );
 }
 
